b842b5642a06355285a537442e0429fc564a988e
gkraud4135 <77388722+gkraud4135@users.noreply.github.com>
2021-03-25 03:41:46 +0900

    Add files via upload

---
+++ Bddd.java
@@ -0,0 +1,40 @@
+package com.coding.book;
+
+public class Bddd {
+
+	public static void main(String[] args) {
+		Tv t = new Tv();
+		t.channer=7;
+		t.power=true;
+		Tv.width = 20;
+		t.channerUp();
+		System.out.println("현재채널"+t.channer);
+		t.gugudan(int dan);
+	}
+
+	
+}
+
+class Tv {
+	
+	String color;
+	boolean power;
+	int channer;
+	
+	static int width =10;
+	static int height =20;
+	
+	void channerUp(){channer++;}
+	void channerDown(){channer--;}
+	void power(){power=!power;}
+	void gugudan(int dan){
+		
+		if(!(2<=dan&&dan<=9)){
+			return;
+		}
+		for(int i=2;i<10;i++){
+			System.out.printf("%d*%d=%d%n",dan,i,dan*i);
+		}return;//void메서드의 return값은 생략가능
+	}
+	
+}
